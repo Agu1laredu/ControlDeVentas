30bef533143d80b27fc01b70bc1872783b5ad467
Eduardo Aguilar <Agu1laredu@hotmail.com>
2023-08-10 23:55:43 -0300

    add ventas pages

--- src/Screen/Ventas/Components/DrowpDown/Dropdown .tsx
+++ src/Screen/Ventas/Components/DrowpDown/Dropdown .tsx
@@ -15,10 +15,18 @@ function VariantsExample() {
           variant={variant.toLowerCase()}
           title={variant}
         >
-          <Dropdown.Item eventKey="1">Ejemplo 1</Dropdown.Item>
-          <Dropdown.Item eventKey="2">Ejemplo 2</Dropdown.Item>
-          <Dropdown.Item eventKey="3">Ejemplo 3</Dropdown.Item>
-          <Dropdown.Item eventKey="4">Ejemplo 4</Dropdown.Item>
+          <Dropdown.Item eventKey="1">
+            <p className="Ejemplos">Ejemplo 1</p>
+          </Dropdown.Item>
+          <Dropdown.Item eventKey="2">
+            <p className="Ejemplos">Ejemplo 2</p>
+          </Dropdown.Item>
+          <Dropdown.Item eventKey="3">
+            <p className="Ejemplos">Ejemplo 3</p>
+          </Dropdown.Item>
+          <Dropdown.Item eventKey="4">
+            <p className="Ejemplos">Ejemplo 4</p>
+          </Dropdown.Item>
         </DropdownButton>
       ))}
     </>
