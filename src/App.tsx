fb86f64e1f42e85f41462991a2d9fbd25901d2d4
Eduardo Aguilar <Agu1laredu@hotmail.com>
2023-10-01 13:16:03 -0300

    add favicon.co

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
   useEffect(() => {
     client.auth.onAuthStateChange((_event, session) => {
       if (!session) {
-        navigate("/login");
+        navigate("/Login");
       } else {
         navigate("/");
       }
